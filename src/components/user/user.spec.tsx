ab547aa1cd8bee3e50fcd4a197878d74681a1b1e
Ivan <ivan@nba-190-74b.local>
2021-07-08 15:34:59 +0300

    first commit

---
+++ src/components/user/user.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import User from './user';
+
+describe('Spec User', function () {
+    it('it exists', () => {
+        expect(User).to.be.ok;
+    });
+});
